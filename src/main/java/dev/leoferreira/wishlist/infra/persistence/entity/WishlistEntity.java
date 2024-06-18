9071945bc6ce42648dad12a4fd159e7516cd5330
leoferreiralima <leoferreiralima@yahoo.com>
2024-06-18 08:29:36 -0300

    perf: add wishlist indexes

--- src/main/java/dev/leoferreira/wishlist/infra/persistence/entity/WishlistEntity.java
+++ src/main/java/dev/leoferreira/wishlist/infra/persistence/entity/WishlistEntity.java
@@ -1,16 +1,23 @@
 package dev.leoferreira.wishlist.infra.persistence.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
 @Document("wishlists")
+@CompoundIndexes({
+        @CompoundIndex(name = "createdBy_productId", def = "{'createdBy' : 1, 'productId': 1}"),
+})
 public class WishlistEntity {
 
     @Id
     private final String id;
     private final String productId;
+    @Indexed
     private final String createdBy;
     private final LocalDateTime createdAt;
 
