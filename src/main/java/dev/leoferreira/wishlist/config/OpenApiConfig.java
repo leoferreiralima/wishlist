95defc934a39aba879ff7a2ad671de332cb9013a
leoferreiralima <leoferreiralima@yahoo.com>
2024-06-19 08:47:38 -0300

    feat: add generic error response to routes of open api

--- src/main/java/dev/leoferreira/wishlist/config/OpenApiConfig.java
+++ src/main/java/dev/leoferreira/wishlist/config/OpenApiConfig.java
@@ -1,7 +1,9 @@
 package dev.leoferreira.wishlist.config;
 
+import dev.leoferreira.wishlist.infra.swagger.openapi.GenericErrorResponseCustomizer;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +17,10 @@ public class OpenApiConfig {
                         .title("Wishlist API")
                         .description("This is a sample Wishlist API built with spring boot, mongodb and redis")
                 );
+    }
+
+    @Bean("genericErrorResponseCustomizer")
+    public OpenApiCustomizer genericErrorResponseCustomizer() {
+        return new GenericErrorResponseCustomizer();
     }
 }
---
+++ src/main/java/dev/leoferreira/wishlist/infra/swagger/openapi/GenericErrorResponseCustomizer.java
@@ -0,0 +1,70 @@
+package dev.leoferreira.wishlist.infra.swagger.openapi;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.media.*;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+
+import java.util.Collection;
+
+public class GenericErrorResponseCustomizer implements OpenApiCustomizer {
+
+    @SuppressWarnings("rawtypes")
+    private final static Schema GENERIC_ERROR_RESPONSE_SCHEMA = new ObjectSchema()
+            .addProperty("timestamp", new DateTimeSchema())
+            .addProperty("status", new IntegerSchema())
+            .addProperty("error", new StringSchema())
+            .addProperty("message", new StringSchema())
+            .addProperty("path", new StringSchema());
+
+    private final static ApiResponse BAD_REQUEST_RESPONSE = new ApiResponse()
+            .description("Bad Request")
+            .content(new Content().addMediaType("application/json", new MediaType().schema(new Schema<>().$ref("#/components/schemas/GenericErrorResponse"))));
+
+    private final static ApiResponse NOT_FOUND_RESPONSE = new ApiResponse()
+            .description("Not Found")
+            .content(new Content().addMediaType("application/json", new MediaType().schema(new Schema<>().$ref("#/components/schemas/GenericErrorResponse"))));
+
+    private final static ApiResponse SERVER_ERROR_RESPONSE = new ApiResponse()
+            .description("Internal Server Error")
+            .content(new Content().addMediaType("application/json", new MediaType().schema(new Schema<>().$ref("#/components/schemas/GenericErrorResponse"))));
+
+    @Override
+    public void customise(OpenAPI openApi) {
+        addGenericErrorResponseSchema(openApi);
+
+        addGenericErrorResponseToOperations(openApi);
+    }
+
+    private void addGenericErrorResponseToOperations(OpenAPI openApi) {
+        openApi.getPaths()
+                .values()
+                .stream()
+                .map(PathItem::readOperations)
+                .flatMap(Collection::stream)
+                .forEach(
+                        operation -> {
+                            ApiResponses responses = operation.getResponses();
+                            responses.addApiResponse("400", NOT_FOUND_RESPONSE);
+                            responses.addApiResponse("404", BAD_REQUEST_RESPONSE);
+                            responses.addApiResponse("500", SERVER_ERROR_RESPONSE);
+                        }
+                );
+    }
+
+    private void addGenericErrorResponseSchema(OpenAPI openApi) {
+        Components components = openApi.getComponents();
+
+        if (components == null) {
+            components = new Components();
+            openApi.setComponents(components);
+        }
+
+        components.addSchemas("GenericErrorResponse", GENERIC_ERROR_RESPONSE_SCHEMA);
+    }
+
+
+}
